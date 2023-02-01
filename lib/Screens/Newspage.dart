181ab0b7d705dd7e4ded1bf75d2c2cb5c8c26c0c
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2023-02-01 22:13:36 +0530

    news page

--- lib/Screens/Newspage.dart
+++ lib/Screens/Newspage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:fluttter_with_test/Screens/ArticlePage.dart';
+import 'package:provider/provider.dart';
+import 'package:fluttter_with_test/Notifiers/NewsChangeNotifier.dart';
 
 class Newspage extends StatefulWidget {
   const Newspage({Key? key}) : super(key: key);
@@ -11,13 +12,52 @@ class Newspage extends StatefulWidget {
 
 class _NewspageState extends State<Newspage> {
   @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => context.read<NewsChangeNotifier>().getArticles(),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('News'),
-        ),
-        body: Container(
-          child: Text("hello"),
-        ));
+      appBar: AppBar(
+        title: const Text('News'),
+      ),
+      body: Consumer<NewsChangeNotifier>(builder: (context, notifier, child) {
+        if (notifier.isLoading) {
+          return const Center(
+            child: CircularProgressIndicator(
+              key: Key('progress-indicator'),
+            ),
+          );
+        }
+        return ListView.builder(
+          itemCount: notifier.articles.length,
+          itemBuilder: (_, index) {
+            final article = notifier.articles[index];
+            return Card(
+              elevation: 2,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => ArticlePage(article: article)));
+                },
+                child: ListTile(
+                  title: Text(article.title),
+                  subtitle: Text(
+                    article.content,
+                    maxLines: 4,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ),
+            );
+          },
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 18),
+        );
+      }),
+    );
   }
 }
